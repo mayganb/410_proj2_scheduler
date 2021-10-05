2681ac70665770a20f9b6af6b39f123c87824270
maygan.brewer.18 <maygan.brewer.18@cnu.edu>
2021-10-05 15:59:46 -0400

    passing

--- scheduler/scheduler_RR.cpp
+++ scheduler/scheduler_RR.cpp
@@ -16,7 +16,7 @@ using namespace std;
 	//override base class behaviour if necessary, otherwise call it
 bool Scheduler_RR::time_to_switch_processes(int tick_count, PCB &p){
 	//(p.required_cpu_time - p.remaining_cpu_time)> 0
-	if((p.required_cpu_time - p.remaining_cpu_time) % time_slice == 0 ){
+	if(p.remaining_cpu_time <= 0 || (p.required_cpu_time - p.remaining_cpu_time) % time_slice == 0 ){
 		return true;
 	}
 	return false;
